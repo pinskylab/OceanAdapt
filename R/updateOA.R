12012a1b368fbdd9cb223768a833629978e83284
rBatt <battrd@gmail.com>
2017-06-20 15:32:15 -0400

    adjust zip script

--- R/updateOA.R
+++ R/updateOA.R
@@ -1,16 +1,17 @@
 # Set the working directory to the top level of the repository (e.g., OceanAdapt/)
 
-dir.create("OAUpdate")
-file.copy("R","OAUpdate", recursive=TRUE)
-file.copy("complete_r_script.R", "OAUpdate")
+# dir.create("OAUpdate")
+# file.copy("R","OAUpdate", recursive=TRUE)
+# file.copy("complete_r_script.R", "OAUpdate")
 
 
 zipFiles <- file.info(list.files("data_updates", full=TRUE, patt="^Data_.+.zip"))
 recentZip <- row.names(zipFiles[order(zipFiles$mtime, zipFiles$ctime, zipFiles$atime, decreasing=TRUE)[1],])
 
-dir.create("OAUpdate/data_updates")
-file.copy(recentZip, "OAUpdate/data_updates", recursive=TRUE)
+# dir.create("OAUpdate/data_updates")
+# file.copy(recentZip, "OAUpdate/data_updates", recursive=TRUE)
 
-zip("OAUpdate", files=list.files(basename("OAUpdate"),full=TRUE))
+# zip("OAUpdate", files=list.files(basename("OAUpdate"),full=TRUE))
+zip("OAUpdate", files=c("R","complete_r_script.R",recentZip))
 
-unlink("OAUpdate", recursive=TRUE)
+# unlink("OAUpdate", recursive=TRUE)
