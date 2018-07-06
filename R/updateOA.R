fd961cf24e393c0ea1c2397a4d048f2b8d9bbc14
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-07-06 13:46:36 -0400

    changed name of zip file to match NJAES requirements and added comments, deleted lines we no longer use.

--- R/updateOA.R
+++ R/updateOA.R
@@ -1,17 +1,12 @@
+# This is a script to package the data to be uploaded to the website
+
 # Set the working directory to the top level of the repository (e.g., OceanAdapt/)
 
-# dir.create("OAUpdate")
-# file.copy("R","OAUpdate", recursive=TRUE)
-# file.copy("complete_r_script.R", "OAUpdate")
-
-
+# get file info (times created, updated) for a list of all of the zip files in the data_updates directory
 zipFiles <- file.info(list.files("data_updates", full=TRUE, patt="^Data_.+.zip"))
+
+# choose the most recent one
 recentZip <- row.names(zipFiles[order(zipFiles$mtime, zipFiles$ctime, zipFiles$atime, decreasing=TRUE)[1],])
 
-# dir.create("OAUpdate/data_updates")
-# file.copy(recentZip, "OAUpdate/data_updates", recursive=TRUE)
-
-# zip("OAUpdate", files=list.files(basename("OAUpdate"),full=TRUE))
-zip("OAUpdate", files=c("R","complete_r_script.R",recentZip))
-
-# unlink("OAUpdate", recursive=TRUE)
+# make a zip file that holds all the info that needs to be sent to the website
+zip("Data_Updated", files=c("R","complete_r_script.R",recentZip))
