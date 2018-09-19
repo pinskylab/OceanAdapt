3bfa3d9c7aca5adee3274c11e69e32a133ce1cc2
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-09-19 10:58:24 -0400

    revert script to the last version Ryan created.

--- R/updateOA.R
+++ R/updateOA.R
@@ -1,12 +1,17 @@
-# This is a script to package the data to be uploaded to the website
-
 # Set the working directory to the top level of the repository (e.g., OceanAdapt/)
 
-# get file info (times created, updated) for a list of all of the zip files in the data_updates directory
-zipFiles <- file.info(list.files("data_updates", full=TRUE, patt="^Data_.+.zip"))
+# dir.create("OAUpdate")
+# file.copy("R","OAUpdate", recursive=TRUE)
+# file.copy("complete_r_script.R", "OAUpdate")
 
-# choose the most recent one
+
+zipFiles <- file.info(list.files("data_updates", full=TRUE, patt="^Data_.+.zip"))
 recentZip <- row.names(zipFiles[order(zipFiles$mtime, zipFiles$ctime, zipFiles$atime, decreasing=TRUE)[1],])
 
-# make a zip file that holds all the info that needs to be sent to the website
-zip("Data_Updated", files=c("R","complete_r_script.R",recentZip))
+# dir.create("OAUpdate/data_updates")
+# file.copy(recentZip, "OAUpdate/data_updates", recursive=TRUE)
+
+# zip("OAUpdate", files=list.files(basename("OAUpdate"),full=TRUE))
+zip("OAUpdate", files=c("R","complete_r_script.R",recentZip))
+
+# unlink("OAUpdate", recursive=TRUE)
