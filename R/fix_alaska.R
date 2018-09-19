444bf00ad399df9d2d2edb33568a069058291d14
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-09-19 12:07:15 -0400

    changed readr::read_csv to read.csv and changed filter to include removing rows with NA for LONGITUDE
    so that files would emerge fixed.

--- R/fix_alaska.R
+++ R/fix_alaska.R
@@ -4,7 +4,7 @@
 
 # As of 2018, some of the Alaska files contain an extra line of header titles buried in the data.  This causes errors in the complete_R_script.  This section finds those extra lines and removes them.
 
-
+library(dplyr)
 dirs <- c("ai", "goa", "ebs")
 for (i in seq(dirs)){
   target <- paste0("data_raw/", dirs[i], "/", Sys.Date())
@@ -12,10 +12,11 @@ for (i in seq(dirs)){
   files <- list.files(dir)
   for (j in seq(files)){
     if(!grepl("strata", files[j])){
-      temp2 <- readr::read_csv(paste0(dir,"/", files[j]))
-      temp2 <- dplyr::filter(temp2, LATITUDE != "LATITUDE")
+      temp2 <- read.csv(paste0(dir,"/", files[j]), stringsAsFactors = F)
+      temp2 <- dplyr::filter(temp2, LATITUDE != "LATITUDE", !is.na(LONGITUDE))
       write.csv(temp2, file = paste0(dir,"/" , files[j]), row.names = F)
     }
   }
 }
- # as code executes, it will flash the warnings of all of the files that have these text headers where there should be numbers, that is a good thing.
+
+# test <- read.csv("data_raw/ai/2018-09-19/ai2014_2016.csv")
