2ffe1a3e6e861b3e2a75eb21f5a012ff7c9a869c
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-09-19 12:16:28 -0400

    create script to remove extra rows from seus data files

---
+++ R/fix_seus.R
@@ -0,0 +1,25 @@
+# Fix SEUS Data 2018 ####
+
+# use this on the raw data before it gets zipped up
+
+# As of 2018, some of the SEUS files contain rows that do not contain data but contain other info that interferes with our scripts.
+
+
+library(dplyr)
+# define the file we are looking for
+target <- paste0("data_raw/seus/", Sys.Date())
+# get the directory
+dir <- list.dirs(target)
+# list the files in that directory
+files <- list.files(dir)
+
+# iterate through the files and remove erroneous lines
+for (j in seq(files)){
+  if(!grepl("strata", files[j])){
+    temp2 <- read.csv(paste0(dir,"/", files[j]), stringsAsFactors = F)
+    temp2 <- dplyr::filter(temp2, PROJECTNAME == "=Coastal Survey")
+    write.csv(temp2, file = paste0(dir,"/" , files[j]), row.names = F)
+  }
+}
+
+# test <- read.csv("data_raw/ai/2018-09-19/ai2014_2016.csv")
