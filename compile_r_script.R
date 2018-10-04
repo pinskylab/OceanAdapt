336a30ef5e60033d4bf07a85d3a3371e09e2c61a
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-10-04 15:07:21 -0400

    add regions

--- compile_r_script.R
+++ compile_r_script.R
@@ -1,2 +1,20 @@
 # The complete_r_script, updated to do all munging in one file, replaceing the data_update.R, updateOA.R, and get_wcann.R scripts.
 
+# setup ####
+library(tidyverse)
+
+# Define Regions of Interest ####
+raw_regions <- tibble(region = c(
+    "ai", #(Aleutian Islands) 
+  "ebs", #(Eastern Bering Sea)
+  "gmex", #(Gulf of Mexico)
+"goa", #(Gulf of Alaska)
+  "neus", #(Northeast US)
+  "seus", #(Southeast US)
+  "taxonomy", #(not a region/ survey, but this folder should exist)
+  "wcann", #(West Coast Annual)
+  "wctri" #(West Coast Triennial)
+))
+
+
+  
