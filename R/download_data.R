0f83a9c9acf56857b36bbb6978b3edb2308f97ee
Michelle Stuart <michelle.stuart@rutgers.edu>
2019-04-03 15:43:40 -0400

    updated the Scotian shelf function to grab 2018 data using the code sent by Tobais. #74

--- R/download_data.R
+++ R/download_data.R
@@ -24,66 +24,48 @@ download_ak <- function(region, ak_files){
 # install_github('Maritimes/FGP')
 # library(FGP)
 
-# FGP doesn't work on this version of R so I'm loading the function here:
-
-#' @title get_DFO_REST
-#' @description This function facilitates the extraction of data from the DFO 
-#' ESRI REST services into R objects and other formats.  
-#' By default, the REST services only allow the extraction of 1000 records at a 
-#' time, and this function does many successive extractions, and merges the
-#' records together into a single object.  
-#' @param service  The default value is
+#' @title pullDFOGIS_Prod
+#' @description This function extracts an r data.frame from the DFO ESRI REST 
+#' service.
+#' @param service  The default value is 
 #' \code{'ADAPT_Canada_Atlantic_Summer_2016'}.  This identifies the service from
 #' which you want to extract data.
-#' @param save_csv The default value is \code{TRUE}, which means that the
-#' extracted data is saved to a csv file in your working directory.  If
-#' \code{FALSE}, no csv will be created.
-#' @param n_rec The default is \code{0}.  Primarily for debugging, this
-#' allows the user to select only some records, rather than doing a full
-#' extraction.
+#' @param save_csv The default value is \code{TRUE}, which means that the 
+#' extracted data is saved to a csv file in your working directory.  If 
+#' \code{FALSE}, no csv will be created. 
+#' @param rec_Start The default is \code{0}.  Primarily for debugging, this 
+#' allows the user to select only some of the records, rather than doing a full 
+#' extraction. 
 #' @family ArcGIS
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @importFrom jsonlite fromJSON
-#' @importFrom utils setTxtProgressBar
-#' @importFrom utils txtProgressBar
-#' @importFrom utils write.csv
 #' @export
-get_DFO_REST <-function(service='ADAPT_Canada_Atlantic_Summer_2016', save_csv=TRUE, n_rec = 0) {
+pullDFOGIS_Prod <-function(service='ADAPT_Canada_Atlantic_Summer_2018', save_csv=TRUE, rec_Start = 0) {
   timer.start=proc.time() #start timing, for science!
-  base_url= paste0('http://geoportal-geoportail.gc.ca/arcgis/rest/services/FGP/',service,'/MapServer/0/')
+  base_url= paste0('http://geoportal-geoportail.gc.ca/arcgis/rest/services/SciencePublicationsCSAS/',service,'/MapServer/0/')
+  require(jsonlite)
   extr_Lim = 1000 #can get this many at a time
   
   #total number of records available
   json_cnt_url <- paste0(base_url,'query?where=1=1&f=pjson&returnCountOnly=true')
-  json_cnt <- jsonlite::fromJSON(json_cnt_url)
+  json_cnt <- fromJSON(json_cnt_url)
   if ('error' %in% names(json_cnt)) stop(paste0('\nSomething appears to be wrong with the selected service. Please try again later.
                                                 \nYou can try visiting the following url in your browser to see if there\'s any additional information:
                                                 \n',base_url))
-  rec_Avail = json_cnt$count
-  cat(paste0(rec_Avail,' records available'))
-  if (n_rec==0){
-    wanted = rec_Avail
-    cat('Starting complete extraction')
-  }else{
-    wanted = n_rec
-    cat(paste0('Starting extraction of first ',n_rec,' records'))
-  }
+  rec_N = json_cnt$count
+  print(paste0('Starting extraction of ',rec_N-rec_Start,' records'))
   
-  pb = txtProgressBar(min=0,
-                      max=ceiling(wanted/extr_Lim),
+  pb = txtProgressBar(min=1, 
+                      max=ceiling((rec_N-rec_Start)/extr_Lim), 
                       style = 3)
-  rec_Start=0
-  for (i in 1:ceiling(wanted/extr_Lim)){
-    if ((wanted-rec_Start) < extr_Lim){
-      extr_Lim<-(wanted-rec_Start)
-    }
+  for (i in 1:ceiling((rec_N-rec_Start)/extr_Lim)){
     this_pull <- paste0(base_url,'query?where=OBJECTID%3E=0&f=pjson&returnCountOnly=false&returnGeometry=false&outFields=*&resultOffset=',rec_Start,'&resultRecordCount=',extr_Lim)
-    this_data <- jsonlite::fromJSON(this_pull)
+    this_data <- fromJSON(this_pull)
     if(i==1){    #first resultset, instantiate df
       this.df=this_data$features$attributes
     }else{       # add to existing df
       this.df = rbind(this.df, this_data$features$attributes)
-    }
+    } 
     rec_Start = rec_Start+extr_Lim
     Sys.sleep(0.1)
     setTxtProgressBar(pb, i)
@@ -93,14 +75,20 @@ get_DFO_REST <-function(service='ADAPT_Canada_Atlantic_Summer_2016', save_csv=TR
   names(this.df) = this_data$fields$alias
   this.df$OBJECTID = NULL
   elapsed=timer.start-proc.time() #determine runtime
-  cat(paste0('\nExtraction of ',nrow(this.df),' records completed in ',round(elapsed[3],0)*-1, ' seconds'))
+  cat(paste0('\nExtraction completed in ',round(elapsed[3],0)*-1, ' seconds'))
   if (save_csv){
-    filename = paste0("data_raw/",service,".csv")
+    filename = paste0(service,'.csv')
     write.csv(this.df,filename, row.names = FALSE)
     cat(paste0("\ncsv written to ",getwd(), filename))
   }
   return(this.df)
 }
+#' Test calls using production 2016 services hosted on DFO eGIS infrastructure
+#' fallNew=pullDFOGIS_Prod(service='ADAPT_Canada_Atlantic_Fall_2018')
+#' summerNew=pullDFOGIS_Prod(service='ADAPT_Canada_Atlantic_Summer_2018')
+# springNew=pullDFOGIS_Prod(service='ADAPT_Canada_Atlantic_Spring_2018')
+
+
 
 
   ## Acquire new data ====
@@ -214,13 +202,13 @@ catch <- list.files(path = "~/Downloads", pattern = "ABUNDANCE", full = T)
 
  # Get Scotian data ====
   # the summer data takes about 10 minutes
-  scot_sumr <- get_DFO_REST(service = "ADAPT_Canada_Atlantic_Summer_2016", save_csv = TRUE)
-  scot_fall <- get_DFO_REST(service = "ADAPT_Canada_Atlantic_Fall_2016", save_csv = TRUE)
-  scot_spr <- get_DFO_REST(service = "ADAPT_Canada_Atlantic_Spring_2016", save_csv = TRUE)
+  scot_sumr <-  pullDFOGIS_Prod(service = "ADAPT_Canada_Atlantic_Summer_2018")
+  scot_fall <-  pullDFOGIS_Prod(service = "ADAPT_Canada_Atlantic_Fall_2018")
+  scot_spr <-  pullDFOGIS_Prod(service = "ADAPT_Canada_Atlantic_Spring_2018")
   
   # This doesn't exist (2017 doesn't exist either)
   # summer2018 <- get_DFO_REST(service = "ADAPT_Canada_Atlantic_Summer_2018", save_csv = TRUE)
-  file.rename("data_raw/ADAPT_Canada_Atlantic_Fall_2016.csv", "data_raw/scot_fall.csv")
-  file.rename("data_raw/ADAPT_Canada_Atlantic_Summer_2016.csv", "data_raw/scot_summer.csv")
-  file.rename("data_raw/ADAPT_Canada_Atlantic_Spring_2016.csv", "data_raw/scot_spring.csv")
+  file.rename("data_raw/ADAPT_Canada_Atlantic_Fall_2018.csv", "data_raw/scot_fall.csv")
+  file.rename("data_raw/ADAPT_Canada_Atlantic_Summer_2018.csv", "data_raw/scot_summer.csv")
+  file.rename("data_raw/ADAPT_Canada_Atlantic_Spring_2018.csv", "data_raw/scot_spring.csv")
   
