c106ec009b0b32bfa622ce6933a1d5bdc2da40c5
Michelle Stuart <michelle.stuart@rutgers.edu>
2020-01-22 06:39:14 -0500

    update download AK file to include directions.

--- R/download_ak.R
+++ R/download_ak.R
@@ -1,4 +1,7 @@
 # This function replaces existing ak files in the data_raw folder with the most recent version.
+
+# Check [Alaskan website](https://www.fisheries.noaa.gov/alaska/commercial-fishing/alaska-groundfish-bottom-trawl-survey-data) for any new data and add it to the list, files to watch are ai2014-2018, ebs2017-2018, and goa2015-2017.  Did the names changes?  Are there more recent files?
+library(tibble)
 download_ak <- function(){
   # define the destination folder
   for (i in seq(ak_files$survey)){
@@ -14,6 +17,26 @@ download_ak <- function(){
     file.remove(file)
   }
 }
+
+ak_files <- tibble(survey = c("ai1983_2000.zip", 
+                              "ai2002_2012.zip", 
+                              "ai2014_2018.zip", 
+                              
+                              "ebs1982_1984.zip", 
+                              "ebs1985_1989.zip", 
+                              "ebs1990_1994.zip", 
+                              "ebs1995_1999.zip", 
+                              "ebs2000_2004.zip", 
+                              "ebs2005_2008.zip", 
+                              "ebs2009_2012.zip", 
+                              "ebs2013_2016.zip", 
+                              "ebs2017_2018.zip", 
+                              
+                              "goa1984_1987.zip", 
+                              "goa1990_1999.zip", 
+                              "goa2001_2005.zip", 
+                              "goa2007_2013.zip", 
+                              "goa2015_2017.zip"))
 
 file_list <- ak_files$survey
 download_ak(file_list)
